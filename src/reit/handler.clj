2c8f46d2b5b134c740f4ef593c4e553fc38011d8
john wesonga <johnwesonga@johns-MacBook-Pro.local>
2020-09-13 17:01:09 -0700

    added coercion deps and updated handler

--- src/reit/handler.clj
+++ src/reit/handler.clj
@@ -1,6 +1,9 @@
 (ns reit.handler
 (:require [reitit.ring :as ring]
           [reit.controller :as ctl]
+          [reitit.coercion.spec]
+          [reitit.coercion.schema]
+          [schema.core :as s]
           [reitit.ring.middleware.parameters :as parameters]
           [ring.middleware.keyword-params :refer [wrap-keyword-params]]))
 
@@ -8,15 +11,17 @@
  [db]
     (ring/ring-handler
         (ring/router
-            [["/" {
+            [["/healthz" {:get (fn [_] {:status 200 :body "healthy"})}]
+            ["/" {
                  :handler #'ctl/default}]
-             ["/greet/:name" {:get {:parameters {:path {:name string?}}}
+             ["/greet/:name" {:get {:parameters {:path {:name s/Str}}}
                                 :handler #'ctl/greet}]]
-            {:data {:middleware [;; query-params & form-params
+            {:data {:coercion reitit.coercion.schema/coercion
+                    :middleware [;; query-params & form-params
                                 parameters/parameters-middleware
                                 wrap-keyword-params]}})
         (ring/routes
             (ring/create-resource-handler
                 {:path "/"})
-           (ring/create-resource-handler
+           (ring/create-default-handler
             {:not-found (constantly {:status 404 :body "Not found"})}))))
